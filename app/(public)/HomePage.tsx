c6d17e73c7fe0adbd618e0efdb506d5dcf2c9716
Lemuel Bonite <lbonite@ouitrend.com>
2025-08-28 22:44:22 -0500

    Starting on the UI

--- app/(public)/HomePage.tsx
+++ app/(public)/HomePage.tsx
@@ -1,4 +1,24 @@
-import React from "react";
+'use client';
+
+import React, { useState, useEffect } from "react";
+import Image from 'next/image';
+import {} from '@mui/material';
+import {} from '@mui/icons-material';
+
+// Interfaces for image data and API responses
+interface ImageItem {
+  id: string;
+  name: string;
+  url: string;
+  uploadedAt: string;
+}
+
+interface ApiResponse {
+  success: boolean;
+  message?: string;
+  images?: ImageItem[];
+  image?: ImageItem;
+}
 
 export default function HomePage() {
   return <div>Home Page</div>;
